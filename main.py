9858c19bbb914d47dac50607077120b19c12edc9
gabrielp-cpu <gabroparraga@gmail.com>
2026-07-15 09:31:27 -0500

    Mi solucion de la calculadora

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+def multiplymultiplenumbers(numbers):
+    if not numbers:
+        return 0
+    resultado = numbers[0]
+    for num in numbers[1:]:
+        resultado *= num
+    return resultado
+
+def isiteven(num):
+    if isinstance(num, int):
+        return num % 2 == 0
+    return False
+
+def isitaninteger(num):
+    return isinstance(num, int)
+
+def main():
+    print("Bienvenido a la super calculadora")
+   
+if __name__ == "__main__":
+    main()
+
